f3e1273a8739e4c448f4bbec0e0c4712d0955c3c
epogrebnyak <e.pogrebnyak@gmail.com>
2018-11-30 13:19:25 +0300

    refactoring of inputs

--- ols.jl
+++ ols.jl
@@ -1,26 +1,45 @@
 using Distributions
 using Statistics
 
+a, b = 0, 20
+β_0 = 0.5
+β = 1
+sd_e = 1.1
+n1 = 100
+s = 50
+
+# TODO: pack this into one tupe
+x_dist1 = Uniform(a, b)
+y_process1(X) = β_0 .+ X * β
+noise_process1(X) = rand(Normal(0, sd_e), size(X, 1), 1)
 
 struct Observation
-    X # enforce n*k
-    Y::Vector # enforce n*1
-end   
+    X::Array # enforce n*k
+    Y::Array #{T,1}
+end 
 
+function make_x(n, x_dist)
+    return rand(x_dist, n, 1)
+end    
+
+function make_observation(X, y_process, noise_process)
+    Y = y_process(x) + noise_process(X)
+    return Observation(X, Y)
+end  
+
+function create_observations(n, x_dist, y_process, noise_process)
+    X = make_x(n, x_dist)
+    Y = y_process(X) + noise_process(X)
+    return Observation(X, Y)
+end  
+
+obs1 = create_observations(n1, x_dist1, y_process1, noise_process1)
 
 struct LinearModel
     obs::Observation
     has_intercept::Bool
     beta
 end    
-
-
-function create_observations(n, x_process, coef, noise)
-    X = x_process(n)    
-    Y = coef.intercept .+ X * coef.slope + map(noise, X)
-    return Observation(X, Y)
-end    
-
 
 # OLS estimation using \
 # https://github.com/giob1994/Alistair.jl/blob/3a11c19150169695581b46e4d1895f0641a4c29d/src/linregress.jl#L38-L41
@@ -39,7 +58,7 @@ function ols_(X, Y)
 end
 
 function add_intercept(X::Array)
-    nrows = size(X)[1]
+    nrows = size(X, 1)
     X0 = ones(nrows, 1)
     return [X0 X]
 end    
@@ -56,43 +75,26 @@ function evaluate_ols(obs; intercept=false)
     return LinearModel(obs, intercept, beta_hat)
 end    
 
-yhat(lm::LinearModel) = lm.obs.X * lm.beta 
-sum_of_squares(x) = sum(x .^ 2) 
-rss(lm::LinearModel) = sum_of_squares(yhat(lm) - lm.obs.Y)
-tss(lm::LinearModel) = sum_of_squares(lm.obs.Y .- mean(lm.obs.Y)) # equals var(Y)*n
-r2(lm::LinearModel) = 1 - rss(lm)/tss(lm)
 #Y_hat = X * beta_hat 
 #RSS = sum((Y_hat - Y) .^ 2)
 #TSS = sum((Y .- mean(Y)) .^ 2) # equals var(Y)*n
 #R2 = 1-(RSS/TSS) 
 
+yhat(lm::LinearModel) = lm.obs.X * lm.beta 
+sum_of_squares(x) = sum(x .^ 2) 
+rss(lm::LinearModel) = sum_of_squares(yhat(lm) - lm.obs.Y)
+tss(lm::LinearModel) = sum_of_squares(lm.obs.Y .- mean(lm.obs.Y)) # equals var(Y)*n
+r2(lm::LinearModel) = 1 - rss(lm)/tss(lm)
 
-a, b = 0, 20
-β_0 = 0.5
-β = 1
-sd_e = 1.1
-n = 100
-s = 50
-
-# normal error process
-#error_process(X) = rand(Normal(0, 0.75), )
-
-
-true_coef = (intercept = β_0, slope = β)
-true_noise(x) = rand(Normal(0, sd_e))
-x_process(n) = rand(Uniform(a, b), n)
-
-obs = create_observations(n, x_process, true_coef, true_noise)
-lm = evaluate_ols(obs, intercept=true)
-
-println("R-squared: ", round(r2(lm), digits=4))
+lm1 = evaluate_ols(obs1, intercept=true)
+println("R-squared: ", round(r2(lm1), digits=4))
 
 # TODO: convert to X arrays
 # TODO: import data
 # NOT TODO: same in R
 # TODO: replicate https://itl.nist.gov/div898/strd/lls/data/LINKS/v-Norris.shtml
 
-# the difference between two methods is really small 
-@assert all(ols(obs.X, obs.Y)-ols_(obs.X,obs.Y) .< 1e-10) # was 14
-@assert add_intercept(collect(1:3)) == [1 1 1; 1 2 3]'
-@assert r2(lm) > .8
+# the difference between two ols methods is really small 
+@assert all(ols(obs1.X, obs1.Y)-ols_(obs1.X,obs1.Y) .< 1e-10) # was 14
+@assert add_intercept(collect(2:4)) == [1 1 1; 2 3 4]'
+@assert r2(lm1) > .8
