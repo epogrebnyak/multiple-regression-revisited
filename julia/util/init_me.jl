00a8cd86ad0759517143cecce292cc140a5bcfa3
epogrebnyak <e.pogrebnyak@gmail.com>
2018-12-06 09:56:13 +0300

    depots listed

--- julia/util/init_me.jl
+++ julia/util/init_me.jl
@@ -1,4 +1,5 @@
-""" Explore Base.load_path()  behaviour in diffferent situations.
+"""
+Explore Base.load_path() behaviour in diffferent situations.
 
 Run this file as:
 
@@ -6,62 +7,36 @@ Run this file as:
     julia --project=@. init_me.jl
     julia --project=. init_me.jl
 
-Try running in a folder that has or does not have a Project.toml file.    
+Try running in a folder that has or does not have Project.toml file.    
 """
 
-MESSAGE = Dict(true => "Exists         ", false => "Does not exist ")
+MESSAGE = Dict(true => " (exists)", false => " (does not exist)")
 exist(path)::Bool = isfile(path) || isdir(path)
-printexist(path::String) = println(MESSAGE[exist(path)], path)
+printexist(path::String) = println("  ", path, MESSAGE[exist(path)])
 printexist(nothing) = println("nothing")
+printf2(s,n=8) = print("  " * s * ' '^(n-length(s)))
  
-println("--project: ")
 p_ = Base.JLOptions().project
 project_option = (p_ != C_NULL) ? unsafe_string(p_) : "option not provided"
-println(project_option)
+println("--project:\n  ", project_option)
 
-println()
 println("load_path():")
 for path in Base.load_path()
     printexist(path)
 end
 
-println()
 println("Base.active_project():")
 printexist(Base.active_project())
-
-println()
 println("Base.current_project():")
 printexist(Base.current_project())
 
+println("alias expansion with Base.load_path_expand():")
+for alias in ["@.", "@", "@stdlib", "@v1.0"] 
+    printf2(alias)
+    printexist(Base.load_path_expand(alias))
+end
 
-# echo 
-# echo lets try activate another environment
-# julia -e"import Pkg; Pkg.activate(ARGS[1]);println(Base.active_project())" "."
-# julia -e"println(isfile(Base.active_project()))" --project=.
-
-# println("Base.DEPOT_PATH collects possible locations for registries") 
-# println("Some locations exist (true), some may not(false)") 
-# for p in Base.DEPOT_PATH println(message[isdir(p)], ": $p") end
-
-
-
-
-
-# julia -e"for path in Base.load_path() println(path) end" --project=.
-
-# REM active directory or nothing
-
-# julia -e"display(Base.load_path_expand(ARGS[1]))" "@"
-# julia -e"import Pkg;Pkg.activate(ARGS[2]); display(Base.load_path_expand(ARGS[1]))" "@" "."
-# REM home directory (if it has a project)
-# julia -e"display(Base.load_path_expand(ARGS[1]))" "@."
-# REM standard library directory
-# julia -e"display(Base.load_path_expand(ARGS[1]))" "@stdlib"
-# REM specific version directory
-# julia -e"display(Base.load_path_expand(ARGS[1]))" "@1.0.2"
-
-# REM back to top directory
-# cd ..
-
-
-
+println("Base.DEPOT_PATH collects possible locations for registries:") 
+for p in Base.DEPOT_PATH 
+    printexist(p)
+end
