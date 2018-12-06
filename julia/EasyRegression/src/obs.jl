82433992b696c7f4cd3eb6dc5100a238f4b07277
E Pogrebnyak <e.pogrebnyak@gmail.com>
2018-12-06 20:59:18 +0300

    abc

--- julia/EasyRegression/src/obs.jl
+++ julia/EasyRegression/src/obs.jl
@@ -2,16 +2,16 @@
 Sample(X,Y)
 
 Sample data type holds X array of size n*k (known as explanatory variables, 
-independent variables, or features) and Y n*1 sized vector (response or dependent variable) 
-for observed data.    
+independent variables, or features) and Y n*1 sized vector (response or 
+dependent variable) for observed data.    
 """
 struct Sample
     X::Array 
     Y::Vector
     function Sample(X, Y)
-        _td(message:: String) = throw(DimensionMismatch(message)) 
-        size(X,1) == size(Y,1) || _td("X and Y must have same number of rows")
-        size(Y,2) == 1 || _td("Y must be a vector: $Y")   
+        td(message:: String) = throw(DimensionMismatch(message)) 
+        size(X,1) == size(Y,1) || td("X and Y must have same number of rows")
+        size(Y,2) == 1 || td("Y must be a vector: $Y")   
         new(X, Y)
     end    
 end
